159ea20d8595a701cc26006197f0502766b16c0c
LaterDisciple05 <akshatshah11111@gmail.com>
2025-01-04 11:04:25 +0530

    2 files added

---
+++ first.java
@@ -0,0 +1,10 @@
+/*Program purpose:-hello world! of java
+Program date:-03/01/2025
+Program author:-Akshat Shah
+Program flaws(if any):-NA*/
+
+public class first{
+    public static void main(String[] args){
+        System.out.println("Akshat Shah");
+    }
+}
---
+++ second.java
@@ -0,0 +1,15 @@
+/*Program purpose:-java program to add 3 numbers 
+Program date:-04/01/2025
+Program author:-Akshat Shah
+Program flaws(if any):-static*/
+
+
+public class second {
+    public static void main(String[] args)
+    {
+        System.out.println("sum of the numbers is =");
+        int a=2,b=3,c=4;
+        int sum=a+b+c;
+        System.out.println(sum);
+    }
+}
